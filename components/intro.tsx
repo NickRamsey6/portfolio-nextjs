f7265e14e1c3d1b68816ee70223dc25e3e262449
Nick Ramsey <nramseysc@gmail.com>
2024-02-05 19:54:29 -0700

    download resume functionality

--- components/intro.tsx
+++ components/intro.tsx
@@ -57,7 +57,7 @@ export default function Intro() {
             <Link href='#Contact' className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>Contact me here 
             <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/></Link>
 
-            <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer'>Download Resume 
+            <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer' href='/Nick Portfolio Resume.pdf' download>Download Resume 
             <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/></a>
 
             <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full'> <BsLinkedin /></a>
