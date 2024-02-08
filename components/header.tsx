675fbc7f42a9d7ca21496b60d1cd130deea35468
Nick Ramsey <nramseysc@gmail.com>
2024-02-08 15:10:36 -0700

    working on active-section-context

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,12 @@
 "use client";
 /* Make this a client component to utilize browser interactivity*/
-import React, { useState } from 'react'
+import React from 'react'
 import { motion } from 'framer-motion';
 import { links } from '@/lib/data';
 import Link from 'next/link';
 import clsx from 'clsx';
 
 export default function Header() {
-    const [activeSection, setActiveSection] = useState('Home');
-
   return (
   <header className='z-[999] relative'>
     <motion.div className='fixed top-0 left-1/2  h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03]
@@ -27,14 +25,14 @@ export default function Header() {
                         {link.name}
                         {
                             link.name === activeSection && (
-                        <motion.span className='bg-gray-100 rounded-full absolute inset-0 -z-10' layoutId='activeSection'
-                        transition={{
-                            type:'spring',
-                            stiffness: 380,
-                            damping: 30,
-                        }}
-                        ></motion.span>
-                        )}
+                                <motion.span className='bg-gray-100 rounded-full absolute inset-0 -z-10' layoutId='activeSection'
+                                transition={{
+                                    type:'spring',
+                                    stiffness: 380,
+                                    damping: 30,
+                                }}
+                                ></motion.span>
+                            )}
                     </Link>
                 </motion.li>
             ))}
