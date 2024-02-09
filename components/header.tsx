95178e3b7ce5d2cc840785475c21e030f2656275
Nick Ramsey <nramseysc@gmail.com>
2024-02-09 11:34:21 -0700

    use custom hook and use effect on about component to set active section from scroll

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 /* Make this a client component to utilize browser interactivity*/
-import React, { useContext } from 'react'
+import React from 'react'
 import { motion } from 'framer-motion';
 import { links } from '@/lib/data';
 import Link from 'next/link';
