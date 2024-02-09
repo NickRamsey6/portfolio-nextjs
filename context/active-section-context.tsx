0bed4b1c9b8f984ee3f937185d02cd2ca35a86fc
Nick Ramsey <nramseysc@gmail.com>
2024-02-09 12:20:44 -0700

    track time of last click to prevent header menu from scrolling through each option unneccessarily

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -12,24 +12,29 @@ type ActiveSectionContextProviderProps = {
 type ActiveSectionContextType = {
     activeSection: SectionName;
     setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
-}
+    timeOfLastClick: number;
+    setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
+};
 
 export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null);
 
 export default function ActiveSectionContextProvider({ children }:
 ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>('Home');
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0); // Tracking this to prevent the header menu from jumping through each option
 
   return (
     <ActiveSectionContext.Provider value={{
         activeSection,
         setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
     }}
     >
         {children}
         </ActiveSectionContext.Provider>
   );
-}
+};
 
 export function useActiveSectionContext() {
     const context = useContext(ActiveSectionContext);
@@ -41,4 +46,4 @@ export function useActiveSectionContext() {
     }
 
     return context;
-}
+};
