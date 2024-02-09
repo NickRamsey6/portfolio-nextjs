e6e665ab493cb073cb46b5066858fd132b3f9384
Nick Ramsey <nramseysc@gmail.com>
2024-02-09 16:22:01 -0700

    initial skills component

---
+++ components/skills.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+import { skillsData } from '@/lib/data'
+
+export default function Skills() {
+  return (
+    <section>
+      <SectionHeading>My skills</SectionHeading>
+      <ul>
+        {
+            skillsData.map((skill, index) => (
+                <li key={index}>{skill}</li>
+            ))}
+      </ul>
+    </section>
+  )
+}
