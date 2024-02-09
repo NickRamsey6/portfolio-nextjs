7be89fe48e2a802684c298716cebbbf32563fc07
Nick Ramsey <nramseysc@gmail.com>
2024-02-09 12:45:26 -0700

    refactor custom hooks and types for cleaner code

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import profilePic from '@/public/pfp.jpg';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
@@ -9,20 +9,10 @@ import { BsArrowRight } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { BsLinkedin } from 'react-icons/bs';
 import { FaGithubSquare } from 'react-icons/fa';
-import { useInView } from 'react-intersection-observer';
-import { useActiveSectionContext } from '@/context/active-section-context';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Intro() {
-    const {ref, inView} = useInView({
-        threshold: 0.5,
-      });
-      const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
-    
-      useEffect(() => {
-        if (inView && Date.now() - timeOfLastClick > 1000) {
-          setActiveSection('Home');
-        }
-      }, [inView, setActiveSection, timeOfLastClick]);
+  const { ref } = useSectionInView('Home', 0.5);
 
   return (
     <section ref={ref} id='home' className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
