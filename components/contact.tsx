07cc6e708332f4fe811cb853201db54dea0be058
Nick Ramsey <nramseysc@gmail.com>
2024-02-09 17:49:07 -0700

    initial contact form styling

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,14 +4,16 @@ import { FaPaperPlane } from 'react-icons/fa'
 
 export default function Contact() {
   return (
-    <section id='contact'>
+    <section id='contact' className='mb-20 sm:mb-28 w-[min(100%,38rem)]'>
       <SectionHeading>Contact me</SectionHeading>
-      <p>Please conact me here through this form or reach me via email at <a className='underline' href='mailto:nramseysc@gmail.com'>nramseysc@gmail.com</a></p>
+      <p className='text-gray-700 text-center'>Please conact me through this form or reach me via email at <a className='underline' href='mailto:nramseysc@gmail.com'>nramseysc@gmail.com</a></p>
 
-      <form>
-        <input type='email' />
-        <textarea />
-        <button type='submit'>Submit <FaPaperPlane /></button>
+      <form className='mt-10 flex flex-col'>
+        <input className='h-14 rounded-lg borderBlack' type='email' />
+        <textarea className='h-52 my-3 rounded-lg borderBlack p-4'/>
+        <button type='submit' className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all'>
+            Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 hover:bg-gray-950 focus:scale-110 hover:scale-110 active:scale-105'/>{' '}
+            </button>
       </form>
     </section>
   )
