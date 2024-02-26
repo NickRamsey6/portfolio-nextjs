b3488c9e8ac321ee76f3ed901e6e69ba2365e606
Nick Ramsey <nramseysc@gmail.com>
2024-02-26 16:48:17 -0700

    setup server side email validation for contact form

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -4,15 +4,35 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const validateString = (value: unknown, maxLength: number) => {
+    if (!value || typeof value !== 'string' || value.length > maxLength) {
+        return false;
+    }
+
+        return true;
+}
+
 export const sendEmail = async (formData: FormData) => {
-    console.log('running on server');
-    console.log(formData.get('senderEmail'));
-    console.log(formData.get('message'));
+    const senderEmail = formData.get('email')
+    const message = formData.get('message');
+
+    // Simple server side validation
+    if (!validateString(senderEmail, 500)) {
+        return {
+            error: 'Invalid sender email'
+        }
+    } 
+    if (!validateString(message, 5000)) {
+        return {
+            error: 'Invalid message'
+        }
+    }
 
     resend.emails.send({
        from: 'onboarding@resend.dev',
        to: 'nramseysc@gmail.com',
        subject: 'Message from contact form',
-       text: 'Hello World!',
+       reply_to: senderEmail,
+       text: message,
     })
   };
--- components/contact.tsx
+++ components/contact.tsx
@@ -21,7 +21,7 @@ export default function Contact() {
           await sendEmail(formData);
         }}>
         <input name='senderEmail' className='h-14 px-4 rounded-lg borderBlack' type='email' required maxLength={500} placeholder='Your email' />
-        <textarea name='message' className='h-52 my-3 rounded-lg borderBlack p-4' required maxLength={500} placeholder='Your message'/>
+        <textarea name='message' className='h-52 my-3 rounded-lg borderBlack p-4' required maxLength={5000} placeholder='Your message'/>
         <button type='submit' className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all'>
             Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 hover:bg-gray-950 focus:scale-110 hover:scale-110 active:scale-105'/>{' '}
             </button>
