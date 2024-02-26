a9743c20cce9347e5af1a795dca75b454b69f100
Nick Ramsey <nramseysc@gmail.com>
2024-02-26 15:32:19 -0700

    create sendEmail action

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,17 +5,10 @@ import SectionHeading from './section-heading';
 import { FaPaperPlane } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { useSectionInView } from '@/lib/hooks';
+import { sendEmail } from '@/actions/sendEmail';
 
 export default function Contact() {
   const { ref } = useSectionInView('Contact');
-
-  const sendEmail = async (formData: FormData) => {
-    'use server';
-
-    console.log('running on server');
-    console.log(formData.get('senderEmail'));
-    console.log(formData.get('message'));
-  };
 
   return (
     <motion.section id='contact' ref={ref} className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center' initial={{opacity: 0,}} whileInView={{opacity: 1,}} transition={{duration: 1}} viewport={{once: true,}}>
