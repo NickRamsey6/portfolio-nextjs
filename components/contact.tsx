c1d7e0a4141d491a0f238bf9434e01eb2488978d
Nick Ramsey <nramseysc@gmail.com>
2024-02-26 23:33:42 -0700

    loading animation for contact form send

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,10 +2,11 @@
 
 import React from 'react';
 import SectionHeading from './section-heading';
-import { FaPaperPlane } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { useSectionInView } from '@/lib/hooks';
 import { sendEmail } from '@/actions/sendEmail';
+
+import SubmitBtn from './submit-btn';
 
 export default function Contact() {
   const { ref } = useSectionInView('Contact');
@@ -22,9 +23,7 @@ export default function Contact() {
         }}>
         <input name='senderEmail' className='h-14 px-4 rounded-lg borderBlack' type='email' required maxLength={500} placeholder='Your email' />
         <textarea name='message' className='h-52 my-3 rounded-lg borderBlack p-4' required maxLength={5000} placeholder='Your message'/>
-        <button type='submit' className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all'>
-            Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 hover:bg-gray-950 focus:scale-110 hover:scale-110 active:scale-105'/>{' '}
-            </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   )
