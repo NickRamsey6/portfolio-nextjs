5f0a3401ebad5e8b8424b56a01ec92958f7e53c4
Nick Ramsey <nramseysc@gmail.com>
2024-02-27 00:10:13 -0700

    add system pref or manual selection useEffect for theme

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsMoon, BsSun } from 'react-icons/bs';
 
 type Theme = 'light' | 'dark';
@@ -11,10 +11,23 @@ export default function ThemeSwitch() {
   const toggleTheme = () => {
     if (theme === 'light') {
         setTheme('dark');
+        window.localStorage.setItem('theme', 'dark');
     } else {
         setTheme('light');
+        window.localStorage.setItem('theme', 'light');
     }
   };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme') as Theme | null;
+
+    if(localTheme) {
+        setTheme(localTheme);
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        setTheme('dark');
+    }
+
+  }, []);
 
   return (
     <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center
