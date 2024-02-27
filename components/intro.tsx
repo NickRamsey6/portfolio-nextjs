d8109d54a24c8a464896769bc0d09770329caaf2
Nick Ramsey <nramseysc@gmail.com>
2024-02-27 00:29:19 -0700

    add styling for intro buttons

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,12 +62,12 @@ export default function Intro() {
             <Link href='#contact' className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>Contact me here 
             <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' onClick={() => {setActiveSection('Contact'); setTimeOfLastClick(Date.now());}}/></Link>
 
-            <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack' href='/Nick Portfolio Resume.pdf' download>Download Resume 
+            <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10' href='/Nick Portfolio Resume.pdf' download>Download Resume 
             <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/></a>
 
-            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack' href='https://linkedin.com/in/nick-wr' target='_blank'> <BsLinkedin /></a>
+            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60' href='https://linkedin.com/in/nick-wr' target='_blank'> <BsLinkedin /></a>
 
-            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack' href='https://github.com/NickRamsey6' target='_blank'> <FaGithubSquare /></a>
+            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60' href='https://github.com/NickRamsey6' target='_blank'> <FaGithubSquare /></a>
         </motion.div>
     </section>
   )
