510ad7681f8890f712f309310e1ab8dd16f1983f
Nick Ramsey <nramseysc@gmail.com>
2024-02-26 23:47:10 -0700

    style footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>
-      <small>
+    <footer className='mb-10 px-4 text-center text-gray-500'>
+      <small className='mb-2 block text-xs'>
         &copy; 2024 Nick. All rights reserved.
       </small>
-      <p>
-        <span>About this website:</span> built with React and Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
+      <p className='text-xs'>
+        <span className='font-semibold'>About this website:</span> built with React and Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
       </p>
     </footer>
   )
