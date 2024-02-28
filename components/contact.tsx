aa2efd9f31d5ceb3f6d51193539c6293caad435b
Nick Ramsey <nramseysc@gmail.com>
2024-02-28 01:08:04 -0700

    styling for contact form in dark mode

--- components/contact.tsx
+++ components/contact.tsx
@@ -14,10 +14,10 @@ export default function Contact() {
   return (
     <motion.section id='contact' ref={ref} className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center' initial={{opacity: 0,}} whileInView={{opacity: 1,}} transition={{duration: 1}} viewport={{once: true,}}>
       <SectionHeading>Contact me</SectionHeading>
-      <p className='text-gray-700 -mt-6'>Please contact me through this form or reach me via email at <a className='underline' href='mailto:nramseysc@gmail.com'>nramseysc@gmail.com</a></p>
+      <p className='text-gray-700 -mt-6 dark:text-white/80'>Please contact me through this form or reach me via email at <a className='underline' href='mailto:nramseysc@gmail.com'>nramseysc@gmail.com</a></p>
 
       <form 
-        className='mt-10 flex flex-col'
+        className='mt-10 flex flex-col dark:text-black'
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
 
@@ -28,8 +28,8 @@ export default function Contact() {
 
           toast.success('Email sent successfully!');
         }}>
-        <input name='senderEmail' className='h-14 px-4 rounded-lg borderBlack' type='email' required maxLength={500} placeholder='Your email' />
-        <textarea name='message' className='h-52 my-3 rounded-lg borderBlack p-4' required maxLength={5000} placeholder='Your message'/>
+        <input name='senderEmail' className='h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none' type='email' required maxLength={500} placeholder='Your email' />
+        <textarea name='message' className='h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none' required maxLength={5000} placeholder='Your message'/>
         <SubmitBtn />
       </form>
     </motion.section>
