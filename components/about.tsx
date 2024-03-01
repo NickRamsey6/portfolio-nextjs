e61e128dd1f310e9a7ce410ec35e22f5272949c6
Nick Ramsey <nramseysc@gmail.com>
2024-02-29 23:47:46 -0700

    second about para

--- components/about.tsx
+++ components/about.tsx
@@ -25,10 +25,9 @@ export default function About() {
       I have used Python and database languages like SQL and SAS. I come from a test driven development background and believe there is no better feeling in programming than passing all your tests!
       </p>
       <p className='mb-3'>
-      It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less
-       normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use 
-       Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by 
-       accident, sometimes on purpose (injected humour and the like).
+      I consider myself the ultimate team player. That means no task or responsibility is too small or beneath my skill level as long as it helps the team. I draw so much energy from being a part of a strong team. Whenever I join a new team,
+      I always start out just looking to learn as much as I can from my teammates. Then I want to find ways that my work can make their lives easier. Whether in a leadership position or just as an individual contributor, I want to look for ways
+      to maximize the talents of all my teammates. I am open to all opportunities, project-based, contract-based or full-time positions. I look forward to my next challenge!
       </p>
     </motion.section>
   )
